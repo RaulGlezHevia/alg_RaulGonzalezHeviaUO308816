af0d1b629422ae505d6d0d622a9a9e50d03569c0
Raúl González Hevia <UO308816@uniovi.es>
2026-02-11 19:52:22 +0100

    trabajo practica 1.2

---
+++ p12/Bucle5.java
@@ -0,0 +1,43 @@
+package p12;
+
+public class Bucle5 {
+
+	public static long bucle4(int n) {
+		long cont = 0;
+		for (int k = 1; k <= n; k++) {
+			for (int z =1; z <=n;z++) {
+				for (int i = 1; i <= n; i=i*2) {
+					for (int j = 1; j <= n; j=j*2) {
+						cont++;
+					}
+				}
+			}
+		}
+		
+		return cont;
+
+	}
+
+	public static void main(String arg[]) {
+		long c = 0;
+		long t1, t2;
+
+		int nVeces = Integer.parseInt(arg[0]);
+
+		System.out.println("n\ttiempo\trepeticiones\tcontador");
+
+		for (int n = 100; n <= 819200; n *= 2) {
+			t1 = System.currentTimeMillis();
+
+			for (int repeticiones = 1; repeticiones <= nVeces; repeticiones++){
+				c = bucle4(n);
+			}
+			t2 = System.currentTimeMillis();
+
+			System.out.println(n + "\t" + (t2 - t1) + "\t" + nVeces + "\t\t" + c);
+
+		} // for
+
+	} // main
+
+} // clase
