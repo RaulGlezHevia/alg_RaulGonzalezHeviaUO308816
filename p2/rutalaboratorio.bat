b998ebc859549f93836a3f1443d222e747e490ac
Raúl González Hevia <UO308816@uniovi.es>
2026-03-04 18:48:49 +0100

    Arreglando el sync tras el cagadón mesopotámico

--- p2/rutalaboratorio.bat
+++ p2/rutalaboratorio.bat
@@ -1,2 +1,2 @@
-set path=C:\Program Files\Java\<jdk>\bin
-set classpath=C:\Users\<loquesea>\<repositorio github>\p12
+set path=C:\Program Files\Java\jdk-22\bin
+set classpath=C:\Users\raulg\Desktop\p2
